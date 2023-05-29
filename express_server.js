e0d7554b5bd1cbc3397faaaed653f12cac54a7f1
Porfirio Mendez Ocampo <porfiriomendez76@gmail.com>
2023-05-29 05:09:35 +0000

    added more views

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
@@ -17,7 +19,8 @@ app.get("/urls.json", (req, res) => {
 
 
 app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+    const templateVars = { greeting: "Hello World!" };
+    res.render("hello_world", templateVars);
 });
 
 
@@ -28,6 +31,16 @@ app.get("/set", (req, res) => {
 
 app.get("/fetch", (req, res) => {
     res.send(`a = ${a}`);
+});
+
+app.get("/urls", (req, res) => {
+    const templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+    res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
